aad8985f19d463225130974e9d702bf37bb5c7ee
dominique cancino <cs.dominique17@gmail.com>
2023-08-21 15:00:26 -0400

    challenge 2

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    @pregunta = params[:pregunta]
+    if params[:pregunta] == "i am going to work"
+      @answer = "Great!"
+    elsif params[:pregunta].end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
